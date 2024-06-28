afbf40c8b559d6338fbef9193be5219baa36b416
Dorsa Rohani <dorsa.rohani@gmail.com>
2024-06-28 12:31:02 -0400

    initial commit

---
+++ model.py
@@ -0,0 +1,13 @@
+
+
+
+# preprocess data
+
+    # remove noise
+    # filters: gaussian filter? median filter? bilateral filter? non local means denoising? anisotropic diffusion?
+    # increase contrast
+    # remove non-lung parts of iamge
+    # resize to 250 x 250 pixel
+    # 
+
+
