adbcd7808feab1942028c778ae5e4b1dfe89223f
Dorsa Rohani <dorsa.rohani@gmail.com>
2024-06-28 15:30:55 -0400

    metadata

---
+++ Model/metadata.py
@@ -0,0 +1,10 @@
+import json
+from Model.architecture import num_classes
+
+metadata = {
+    "architecture": "LungCancerCNN",
+    "num_classes": num_classes,
+}
+
+with open('model_metadata.json', 'w') as f:
+    json.dump(metadata, f)
---
+++ README.md
@@ -0,0 +1,1 @@
+python Model/architecture.py
