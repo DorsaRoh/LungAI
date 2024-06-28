f6157b97319ba10d9319631f450b9f8c1c5d5bd8
Dorsa Rohani <dorsa.rohani@gmail.com>
2024-06-28 15:33:05 -0400

    update requirements + onnx conversion

--- Model/convert_to_onnx.py
+++ Model/convert_to_onnx.py
@@ -1,10 +1,19 @@
-import json
-from Model.architecture import num_classes
+import torch
+import onnx
+from model import LungCancerCNN  # ensure this imports your model definition
 
-metadata = {
-    "architecture": "LungCancerCNN",
-    "num_classes": num_classes,
-}
+# load your trained model
+model_path = 'lung_cancer_detection_model.pth'
+num_classes = 4  
+model = LungCancerCNN(num_classes)
+model.load_state_dict(torch.load(model_path))
+model.eval()
 
-with open('model_metadata.json', 'w') as f:
-    json.dump(metadata, f)
+input = torch.randn(1, 3, 250, 250)
+
+# export the model to ONNX format
+onnx_model_path = 'lung_cancer_detection_model.onnx'
+torch.onnx.export(model, input, onnx_model_path, 
+                  input_names=['input'], output_names=['output'],
+                  dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})
+print(f'Model successfully converted to {onnx_model_path}')
